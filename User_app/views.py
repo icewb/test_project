86d4f60620245d1817a84c748894e261c977067e
icewu <ice_wb@126.com>
2018-10-11 12:40:39 +0800

    第二节提交

--- User_app/views.py
+++ User_app/views.py
@@ -14,12 +14,15 @@ def index(request):
 
 #处理登陆请求
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username","")
+        password = request.POST.get("password","")
 
         if username == "" or password == "":
            #return HttpResponse("用户名或密码不能为空")
            return render(request,"index.html",{"error":"用户名或密码不能为空啊"})
+        
+        if username == "admin" and password == "123456":
+            return render(request,"project_manager.html",{"messange":"欢迎进入管理页面"})
 
 
